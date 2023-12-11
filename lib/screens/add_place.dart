dcad39e96165ee40a4c09c61c70b275b4ef11e67
gustawx <haslO1234?>
2023-12-11 22:46:12 +0100

    feat: add image input functionality to add_place screen
    - Add image_picker dependency to pubspec.yaml for image selection
    - Create new ImageInput widget in image_input.dart for image selection and display
    - Add ImageInput widget to add_place.dart screen
    - Update Info.plist with necessary permissions for image selection
    - Register file_selector plugin for Linux, MacOS, and Windows platforms for file selection support

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:native_features/providers/user_places.dart';
+import 'package:native_features/widgets/image_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -49,6 +50,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 16),
+            const ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
